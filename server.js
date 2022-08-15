3530c902316ccc04981ef6b90a443efa8ab3c931
Maycon <maycon.trb@gmail.com>
2022-08-15 10:27:13 -0300

    Configuração para levantar a aplicação no heroku

---
+++ server.js
@@ -0,0 +1,15 @@
+// Importa as dependencias que acabamos de intalar
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve os arquivos estáticos da pasta dist (gerada pelo no build)
+app.use(express.static(__dirname + "/dist/forum"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname + "dist/forum/index.html"));
+});
+
+//Inicia a aplicação pela porta configurada
+app.listen(process.env.PORT || 8080);
